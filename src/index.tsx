9bfed33f89cdade1b1393fb417d66eb8ba6da943
Uni <1025687605@qq.com>
2019-08-08 11:10:09 +0800

    feat: add

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import Provider from './provide'
+import combineReducers from './combineReducers'
+import useSelector from './useSelector'
+import useStore from './useStore'
+
+export { Provider, combineReducers, useSelector, useStore }
