d6a098ea4834cacd5d9229f0954705921667a863
Uni <1025687605@qq.com>
2019-08-21 10:49:23 +0800

    fix: ts

--- src/applyMiddleware.tsx
+++ src/applyMiddleware.tsx
@@ -7,7 +7,7 @@ import { compose } from './hookUtil'
 export default function applyMiddleware(...middlewares: any[]) {
   return (createStore: any) => (...args: any[]) => {
     const store = createStore(...args)
-    let dispatch = (...args: any[]) => {
+    let dispatch = (..._args: any[]) => {
       throw new Error(
         'Dispatching while constructing your middleware is not allowed. ' +
           'Other middleware would not be applied to this dispatch.'
